7f228d2af3b4bc3fa20ebccdd5380943becb738e
Computer <gabecadiz@hotmail.com>
2019-07-06 18:20:38 -0400

    added font and background styling

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ class App extends Component {
     )
     return (
       <div className="App">
+        <h1>Monster Mash</h1>
         <SearchBox
           placeholder='Search Monsters'
           handleChange={this.handleChange}
