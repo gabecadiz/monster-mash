4a14af16fe31dc23179a16efd0cafe5646a15e2a
Computer <gabecadiz@hotmail.com>
2019-07-06 18:28:34 -0400

    Updates

---
+++ precache-manifest.c7ca0c81fe8c3a90f08fd088cbfa0d1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b79d8de1ce80a682d90a3699dd2aa29d",
+    "url": "/monster-mash/index.html"
+  },
+  {
+    "revision": "16d94cf89ecb3bf629b2",
+    "url": "/monster-mash/static/css/main.1e490ae7.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-mash/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "16d94cf89ecb3bf629b2",
+    "url": "/monster-mash/static/js/main.0653e1c0.chunk.js"
+  },
+  {
+    "revision": "372fdc061f3046ac5df5",
+    "url": "/monster-mash/static/js/runtime~main.0f322cb0.js"
+  }
+]);
